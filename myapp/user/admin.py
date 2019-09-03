b5664b63aa082243d521fc452999debe07eaaadb
kimevanjunseok <jnunseok@gmail.com>
2019-09-03 17:38:53 +0900

    admin.py modified

--- myapp/user/admin.py
+++ myapp/user/admin.py
@@ -3,6 +3,6 @@ from myapp.home.models import Boards
 
 # Register your models here.
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'finished_at',)
+    list_display = ('title', 'content', 'priority', 'created_at', 'finished_at',)
 
 admin.site.register(Boards, BoardAdmin)
